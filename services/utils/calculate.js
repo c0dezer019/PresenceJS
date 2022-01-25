433a3013fc05062ecbe68485444bfed730678a5f
Brian Blankenship <c0dezer019@zohomail.com>
2022-01-25 00:46:53 -0600

    Calculate stuff, showing Einstein who's boss.

---
+++ services/utils/calculate.js
@@ -0,0 +1,20 @@
+const { intervalToDuration } = require('date-fns');
+
+export const timeIdle = datetime => {
+    const currentTime = new Date();
+    const timeDelta = Math.abs(currentTime - datetime); // Returns an a time delta in milliseconds.
+    const duration = intervalToDuration({ start: 0, end: timeDelta });
+    const timeStr = []
+
+    // Returns something like 0 years, 10 months, 24 days, 0 hours, 1 minutes, and 25 seconds
+    Object.entries(duration).map(([key, value]) => timeStr.push(`${value} ${key}`));
+    timeStr[timeStr.length - 1] = `and ${timeStr[timeStr.length - 1]}.` // Got to be grammatically correct, right
+
+    return timeStr.join(', ').trim();
+};
+
+export const avg = arrOfValues => {
+    const reducer = (last, current) => last + current;
+    const sum = arrOfValues.reduce(reducer);
+    return sum / arrOfValues.length;
+};
