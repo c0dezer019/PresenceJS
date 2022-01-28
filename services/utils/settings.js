8c912d0c45b84685e5c57b1517293c68e1c3daa4
Brian Blankenship <c0dezer019@zohomail.com>
2022-01-28 16:31:06 -0600

    Adding new settings for experimentation.

--- services/utils/settings.js
+++ services/utils/settings.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 
+// Activity gauges are experimental.
 module.exports = {
   bot_owner: process.env.BOT_OWNER,
+  veryActive: [0, 30], // hours, minutes. Guild is considered very active when avg is within specified range.
+  active: [1, 0],
+  moderatelyActive: [3, 0],
+  onItsWayOut: [24, 0],
+  dead: [48, 0]
 };
