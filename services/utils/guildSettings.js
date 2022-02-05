b204c7b73b74b1b79a3ed6ee520894da6e0d4bde
Brian Blankenship <c0dezer019@zohomail.com>
2022-02-05 16:50:35 -0600

    Adding two new settings.

--- services/utils/guildSettings.js
+++ services/utils/guildSettings.js
@@ -5,4 +5,6 @@ module.exports = {
   kickTimer: [1, 0], // How long until member has exceeded time allowed idle.
   AWOL: [], // List of member IDs that have exceeded their allowed idleness (only used when autoKick is off).
   exempt: [], // List of member IDs that have been exempted from autoKicking or going AWOL.
+  timezone: "UTC", // What timezone to convert DateTimes to. Global default if member does not have one set.
+  locale: "en", // Locale of the guild. Global default for all members that do not have locale set.
 }
