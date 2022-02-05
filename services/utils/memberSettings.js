061038e7c6312403dd79964bb8cc6da65fd0806c
Brian Blankenship <c0dezer019@zohomail.com>
2022-02-05 16:44:05 -0600

    User specific settings.

---
+++ services/utils/memberSettings.js
@@ -0,0 +1,4 @@
+module.exports = {
+    timezone: "", // Timezone to convert DateTimes to. Overrides guild timezone setting when set;
+    locale: "", // What language to display datetime in. Overrides guild locale when set.
+}
